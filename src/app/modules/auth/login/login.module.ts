4a8fa35d3c570671063add27b82401862433dffe
ilham <ilham@atarasaqt.com>
2026-01-21 10:24:42 +0700

    feat: new page login

--- src/app/modules/auth/login/login.module.ts
+++ src/app/modules/auth/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { IonicModule } from '@ionic/angular';
 
 const routes: Routes = [
   {
@@ -15,6 +16,7 @@ const routes: Routes = [
   declarations: [LoginComponent],
   imports: [
     CommonModule,
+    IonicModule,
     RouterModule.forChild(routes)
   ]
 })
